3412c44badd708838048072dde4ce6c377df28b7
Felix-Mutinda <1mutindafelix@gmail.com>
2019-08-06 13:12:11 +0000

    add base/home templates

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    '''
+    handles requests to the home page.
+    '''
+    return render(request, 'core/home.html')
--- hrms/settings.py
+++ hrms/settings.py
@@ -121,5 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # use custom auth model
 AUTH_USER_MODEL = 'core.User'
