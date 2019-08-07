0c2991170044bf4c348ce140171e592c27b865e3
Felix-Mutinda <1mutindafelix@gmail.com>
2019-08-07 14:22:58 +0000

    register models to admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import User, Employer, Employee, Asset, AssignedAsset
+
+admin.site.register(User)
+admin.site.register(Employer)
+admin.site.register(Employee)
+admin.site.register(Asset)
+admin.site.register(AssignedAsset)
